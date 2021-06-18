a6a4136a781339002d4c77790a6a20e2fb13733e
Joseph Hilber <joeyhilber@gmail.com>
2021-06-18 11:39:26 -0400

    added color themes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import "./index";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
-import Home from "./Home";
 import Footer from "./Footer";
+import Home from "./Home";
 import Projects from "./Projects";
-import Contact from "./Contact";
 import NotFound from "./NotFound";
-import "./index.scss";
-// import { useEffect, useState } from "react";
+import GlobalStyle from "./Global";
 
 function App() {
-
-  // const [color, setColor] = useState("white");
-
-  // useEffect(() => {
-  //   // Update the document title using the browser API
-  //   console.log("hello I'm alive");
-  //   var colors = ["blue", "green", "orange", "tan", "red", "purple"];
-  //   var randomColor = colors[Math.floor(Math.random() * colors.length)];
-  //   console.log(randomColor);
-  //   // App.style.setProperty("background-color", randomColor);
-  //   setColor(randomColor);
-  // }, []);
-
   return (
     <Router>
-      {/* <div className="App" style={{ backgroundColor: `${color}` }}> */}
-      {/* <div className={`App ${color}`}> */}
+      <GlobalStyle />
       <div className="App">
         <Navbar />
         <div className="content">
@@ -37,9 +21,6 @@ function App() {
             </Route>
             <Route exact path="/projects">
               <Projects />
-            </Route>
-            <Route exact path="/contact">
-              <Contact />
             </Route>
             <Route path="*">
               <NotFound />
--- src/Contact.js
+++
@@ -1,7 +0,0 @@
-const Contact = () => {
-    return (
-        <div>testing Contact</div>
-    );
-}
-
-export default Contact;
